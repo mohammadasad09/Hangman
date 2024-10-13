92fb9f690e009417c9b79bda27f82f8cab98b2ed
Mohammad Asad <mohammadasadisb@gmail.com>
2024-10-14 01:09:46 +0500

    Add files via upload
    Customized word lists for specific categories.

---
+++ geography.py
@@ -0,0 +1,115 @@
+easy = [
+    "New York", "Los Angeles", "London", "Paris", "Tokyo", "Beijing", "Cairo", 
+    "Sydney", "Moscow", "Berlin", "Rome", "Toronto", "Mexico City", "Mumbai", 
+    "Dubai", "Jerusalem", "Rio de Janeiro", "Madrid", "Bangkok", "Istanbul", 
+    "Washington D.C.", "Chicago", "Houston", "Buenos Aires", "Sao Paulo", 
+    "Lima", "Seoul", "Brussels", "Vienna", "Stockholm", "Amsterdam", "Melbourne", 
+    "Johannesburg", "Cape Town", "Lisbon", "Athens", "Milan", "Manila", 
+    "Singapore", "Zurich", "Barcelona", "Jakarta", "Kuala Lumpur", "Auckland", 
+    "Shanghai", "Hong Kong", "Brisbane", "Montreal", "Helsinki", "Oslo", 
+    "Reykjavik", "Dublin", "Warsaw", "Prague", "Munich", "Florence", "Naples", 
+    "Lyon", "Casablanca", "Rabat", "Doha", "Tel Aviv", "Alexandria", "Havana", 
+    "Panama City", "Lagos", "Abuja", "Accra", "Addis Ababa", "Nairobi", "Colombo",
+    "Buenos Aires", "Rio de Janeiro", "Caracas", "La Paz", "Montevideo", 
+    "Havana", "Kingston", "Mexico City", "Santo Domingo", "San Juan", 
+    "New Delhi", "Bangalore", "Chennai", "Hyderabad", "Kolkata", "Islamabad", 
+    "Karachi", "Lahore", "Sydney", "Melbourne", "Brisbane", "Perth", "Canberra", 
+    "Christchurch", "Auckland", "Wellington", "Toronto", "Vancouver", "Montreal", 
+    "Ottawa", "Brazil", "Argentina", "Chile", "Colombia", "Peru", "United States", 
+    "United Kingdom", "France", "Germany", "China", "Russia", "Australia", 
+    "Canada", "Spain", "Italy", "Japan", "South Korea", "India", "Brazil", 
+    "South Africa", "Mexico", "Turkey", "Israel", "Saudi Arabia", "Qatar", 
+    "Egypt", "Morocco", "Thailand", "Vietnam", "Malaysia", "Philippines", 
+    "Indonesia", "Singapore", "Sri Lanka", "Bangladesh", "Pakistan", "Nigeria", 
+    "Ghana", "Kenya", "Ethiopia", "Tanzania"
+]
+
+
+medium = [
+    "Edinburgh", "Glasgow", "Manchester", "Birmingham", "Munich", "Frankfurt", 
+    "Hamburg", "Dublin", "Cork", "Valencia", "Bilbao", "Florence", "Venice", 
+    "Naples", "Lyon", "Marseille", "Nice", "Lagos", "Abuja", "Durban", 
+    "Brisbane", "Perth", "Adelaide", "Vancouver", "Ottawa", "Santiago", 
+    "Valparaiso", "Havana", "Quito", "Guayaquil", "Casablanca", "Marrakesh", 
+    "Tangier", "Tel Aviv", "Haifa", "Salzburg", "Graz", "Zurich", "Geneva", 
+    "Basel", "Antwerp", "Ghent", "Rotterdam", "The Hague", "Luxembourg City", 
+    "Bologna", "Seville", "Porto", "Funchal", "Belgrade", "Sofia", "Zagreb", 
+    "Tallinn", "Vilnius", "Riga", "Tbilisi", "Baku", "Yerevan", "Astana", 
+    "Colombo", "Kandy", "Kathmandu", "Pokhara", "Bandung", "Surabaya", 
+    "Chiang Mai", "Phnom Penh", "Vientiane", "Hanoi", "Ho Chi Minh City", 
+    "Manila", "Cebu", "Davao", "Abuja", "Kampala", "Harare", "Nairobi", 
+    "Dar es Salaam", "Maputo", "Lusaka", "Gaborone", "Windhoek", "Minsk", 
+    "Skopje", "Podgorica", "Sarajevo", "Vilnius", "Riga", "Bratislava", "Ljubljana", 
+    "Bishkek", "Tashkent", "Ashgabat", "Almaty", "Monrovia", "Conakry", 
+    "Accra", "Mogadishu", "Banjul", "Managua", "Tegucigalpa", "La Paz", 
+    "Asuncion", "Antananarivo", "Sao Tome", "Nouakchott", "Malabo", "Port Louis", 
+    "Fiji", "Vanuatu", "Bahamas", "Barbados", "Trinidad and Tobago", "Luxembourg", 
+    "Belgium", "Austria", "Switzerland", "Portugal", "Netherlands", "Denmark", 
+    "Norway", "Sweden", "Finland", "Hungary", "Poland", "Czech Republic", 
+    "Slovakia", "Serbia", "Croatia", "Romania", "Bulgaria", "Slovenia", 
+    "Latvia", "Lithuania", "Estonia", "Georgia", "Azerbaijan", "Kazakhstan", 
+    "Armenia", "Belarus", "Uzbekistan", "Tajikistan", "Turkmenistan", 
+    "Kyrgyzstan", "Albania", "Moldova", "Montenegro", "Macedonia", 
+    "Ivory Coast", "Zambia", "Malawi", "Mozambique", "Botswana", "Eswatini"
+]
+
+
+difficult = [
+    "Skopje", "Podgorica", "Sarajevo", "Vilnius", "Riga", "Reykjavik", "Valletta", 
+    "Ljubljana", "Pristina", "Chisinau", "Minsk", "Tirana", "Gaborone", "Windhoek", 
+    "Ouagadougou", "Lome", "Port Louis", "Victoria", "Nouakchott", "Thimphu", 
+    "Suva", "Apia", "Palikir", "Malabo", "Banjul", "Bissau", "Niamey", "Freetown", 
+    "Monrovia", "Conakry", "Bujumbura", "Gitega", "Libreville", "Yaounde", "Bangui", 
+    "Brazzaville", "Kinshasa", "Djibouti", "Juba", "Honiara", "Funafuti", 
+    "Kigali", "Vientiane", "Phnom Penh", "Manama", "Bishkek", "Dushanbe", 
+    "Ashgabat", "Kampala", "Antananarivo", "Mogadishu", "Banjul", "Ouagadougou", 
+    "Bissau", "Freetown", "Bujumbura", "Brazzaville", "Port Moresby", 
+    "Lusaka", "Gaborone", "Windhoek", "N'Djamena", "Bamako", "Nouakchott", 
+    "Monrovia", "Conakry", "Praia", "Port Louis", "Victoria", "Georgetown", 
+    "Paramaribo", "Belmopan", "Port of Spain", "Kingstown", "Bridgetown", 
+    "Castries", "Port Vila", "Honiara", "Tarawa", "Funafuti", "Nuku'alofa", 
+    "Apia", "Majuro", "Palikir", "Tuvalu", "Vanuatu", "Malta", "Monaco", 
+    "Andorra", "San Marino", "Lesotho", "Eswatini", "Guinea", "Mali", 
+    "Burkina Faso", "Togo", "Benin", "Chad", "Niger", "Eritrea", "Djibouti"
+]
+
+
+
+professional = [
+    "Bishkek", "Dushanbe", "Ashgabat", "Ulaanbaatar", "Yamoussoukro", "Dodoma", 
+    "Lilongwe", "Bujumbura", "Bamako", "N'Djamena", "Ouagadougou", "Freetown", 
+    "Banjul", "Conakry", "Monrovia", "Juba", "Malabo", "Nouakchott", "Thimphu", 
+    "Suva", "Honiara", "Apia", "Tarawa", "Funafuti", "Majuro", "Palikir", 
+    "Praia", "Paramaribo", "Belmopan", "Port Louis", "Victoria", "Georgetown", 
+    "Nuku'alofa", "Port Vila", "Vanuatu", "Kiribati", "Nauru", "Marshall Islands", 
+    "Micronesia", "Tuvalu", "Mauritius", "Lesotho", "Eswatini", "Comoros", 
+    "Seychelles", "Sao Tome", "Cabo Verde", "Djibouti", "Eritrea", "Equatorial Guinea", 
+    "Gabon", "Guinea-Bissau", "Sierra Leone", "Mali", "Burkina Faso", 
+    "Central African Republic", "Togo", "Benin", "Chad", "Niger", "Liberia", 
+    "Sudan", "South Sudan", "Zanzibar", "Tanzania", "Malawi"
+]
+
+
+impossible = [
+    "Ngerulmud", "Koror", "Melekeok", "Ngardmau", "Ngaraard", "Vaitupu", 
+    "Nanumea", "Nukufetau", "Isangel", "Lakatoro", "Sola", "Buala", 
+    "Tulagi", "Kirakira", "Auki", "Gizo", "Fuvahmulah", "Thinadhoo", 
+    "Kulhudhuffushi", "Naifaru", "Kaédi", "Zouerate", "Rosso", "Atar", 
+    "Gbarnga", "Kakata", "Zwedru", "Voinjama", "Hlotse", "Quthing", 
+    "Mohale's Hoek", "Teyateyaneng", "Mafeteng", "Ruyigi", "Kayanza", 
+    "Bururi", "Ngozi", "Fada N’Gourma", "Banfora", "Ouahigouya", 
+    "Gitega", "Kayes", "Koutiala", "Sikasso", "Mopti", "Segou", 
+    "Nampula", "Matola", "Quelimane", "Chimoio", "Am Timan", 
+    "Sarh", "Abeche", "Kelo", "Tahoua", "Dosso", "Maradi", 
+    "Agadez", "Zinder", "Moundou", "Labasa", "Ba", "Levuka", 
+    "Nadi", "Labasa", "Levuka", "Gizo", "Kirakira", "Kolovai", 
+    "Mu’a", "Pangai", "Hihifo", "Atakpamé", "Dapaong", "Tsévié", 
+    "Sokodé", "Kara", "Tarrafal", "Espargos", "Santa Maria", "Bohicon", 
+    "Natitingou", "Abomey", "Parakou", "Amman", "Madaba", "Karak", 
+    "Jerash", "Ajloun", "Aqaba", "Zarqa", "Ma'an", "Balqa", 
+    "Irbid", "Tafilah", "Tlemcen", "Blida", "Annaba", "Skikda", 
+    "Batna", "Constantine"
+]
+
+
+geography = [easy, medium, difficult, professional, impossible]
---
+++ movies.py
@@ -0,0 +1,122 @@
+easy = [
+    "Action", "Adventure", "Actor", "Actress", "Adaptation", "Audience", 
+    "Award", "Background", "Blockbuster", "Box office", "Camera", "Cast", 
+    "Character", "Cinematographer", "Comedy", "Costume", "Credits", 
+    "Director", "Drama", "Effects", "Ending", "Epic", "Extras", "Fantasy", 
+    "Feature", "Film", "Flashback", "Focus", "Footage", "Genre", "Hero", 
+    "Hollywood", "Horror", "Iconic", "Independent", "Lead", "Lighting", 
+    "Location", "Makeup", "Montage", "Musical", "Mystery", "Opening", "Oscar", 
+    "Plot", "Premiere", "Producer", "Production", "Protagonist", "Rating", 
+    "Reboot", "Remake", "Release", "Scene", "Script", "Sequel", "Set", 
+    "Setting", "Shot", "Show", "Soundtrack", "Special effects", "Spoiler", 
+    "Spotlight", "Stunt", "Studio", "Supporting", "Take", "Talent", 
+    "Teaser", "Theme", "Thriller", "Ticket", "Title", "Trailer", "Villain", 
+    "Voiceover", "Western", "Zoom", "Animation", "Camera operator", 
+    "Character development", "Choreography", "Climax", "Cold open", "Composer", 
+    "Costume drama", "Cult classic", "Documentary", "Dubbing", "Editor"
+]
+
+
+medium = [
+    "Adaptation", "Antagonist", "Aisle", "Audition", "Backstage", "Cameo", 
+    "Casting", "Close-up", "Climax", "Cinematography", "Cut", "Dubbing", 
+    "Ensemble", "Flashback", "Focus", "Foley", "Framing", "Green screen", 
+    "Improv", "Industry", "Intermission", "Lighting", "Montage", "Narrative", 
+    "Pacing", "Plot twist", "Premiere", "Prop", "Prosthetics", "Rehearsal", 
+    "Script", "Set decorator", "Shot list", "Silent film", "Sound mixing", 
+    "Spotlight effect", "Static shot", "Steadicam", "Story arc", "Subtitle", 
+    "Synopsis", "Table read", "Tagline", "Test audience", "Third act", 
+    "Three-act structure", "Tracking shot", "Trailer music", "Treatment", 
+    "Underexposure", "Vignette", "Walk-on role", "Wardrobe", "Wide release", 
+    "Zoom lens", "Actor's guild", "Auteur", "Biopic", "Boom mic", "Camera angle", 
+    "Canon", "Character arc", "Clapperboard", "Climax scene", "Color grading", 
+    "Composer", "Costume designer", "Crosscutting", "Deleted scene", 
+    "Digital effects", "Docudrama", "Dolly shot", "Establishing shot", 
+    "Extreme close-up", "Fade-in", "Fade-out", "Film adaptation", "Film critic", 
+    "Film score", "Film festival", "Film noir", "Genre film", "Guerrilla filmmaking", 
+    "Handheld camera", "High-concept", "Key light", "Long shot", "MacGuffin", 
+    "Medium shot", "Mini-series", "Mockumentary", "POV shot", "Pan shot", 
+    "Parallel editing", "Pre-production", "Principal photography", "Pyrotechnics", 
+    "Reaction shot", "Red carpet", "Reshoot", "Romantic comedy", "Screen test", 
+    "Silent film", "Slapstick", "Slow motion", "Storyboard", "Supporting cast", 
+    "Technical award", "Telephoto lens", "Third act twist", "Time-lapse", 
+    "Tracking shot", "Underexposed", "Vanity project"
+]
+
+difficult = [
+    "Adaptation", "Aerial shot", "Antihero", "Art director", "Aspect ratio", 
+    "Auteur", "Backlot", "Backstory", "Boom operator", "BTS (Behind The Scenes)", 
+    "Camera angle", "Camera operator", "Cinemascope", "Cliffhanger", "Cold open", 
+    "Color correction", "Costume drama", "Crossfade", "Dolly grip", "Drive-in", 
+    "End credits", "Featurette", "Filler episode", "Film analysis", 
+    "Filmography", "First assistant", "Flipbook", "Freeze-frame", "Golden Globes", 
+    "Guerrilla filmmaking", "Hand-drawn animation", "Hero’s journey", "Jump scare", 
+    "Lens distortion", "Low-budget", "Lynchian", "Magic realism", "Master shot", 
+    "Meta-narrative", "Micro-budget", "Montage sequence", "Narration", 
+    "New wave", "Niche audience", "Non-diegetic", "Off-screen", "Over-cranked", 
+    "Panoramic", "Parabolic mic", "Pitch meeting", "Post-production", 
+    "Production designer", "Projector", "Prosthetics", "Psychological thriller", 
+    "Quasi-documentary", "Re-enactment", "Rotoscope", "Rushes", "Screen actor", 
+    "Second unit", "Series regular", "Shooting script", "Short film", "Slate", 
+    "Sound design", "Split screen", "Steadicam operator", "Storyboard artist", 
+    "Test screening", "Title card", "Tracking shot", "Tropes", "Underexposed shot", 
+    "Vanity project", "Visual storytelling", "Widescreen", "Zoom-in", "Zoom-out"
+]
+
+professional = [
+    "Aerial shot", "Antihero", "Art director", "Aspect ratio", "Auteur", 
+    "Background artist", "Boom mic", "Boom operator", "BTS (Behind The Scenes)", 
+    "Camera angle", "Camera operator", "Cinemascope", "Cliffhanger", "Color correction", 
+    "Crossfade", "Dolly grip", "Drive-in", "Extreme close-up", "Foley artist", 
+    "Golden Globes", "Guerrilla filmmaking", "Hand-drawn animation", 
+    "Hero’s journey", "Jump scare", "Key grip", "Lavalier mic", "Lens distortion", 
+    "Location scout", "Low-budget", "Lynchian", "MacGuffin", "Matte painting", 
+    "Medium shot", "Miniature", "Mockumentary", "Mise en scène", "Narration", 
+    "New wave", "Non-diegetic", "One-liner", "On-location", "Over-cranked", 
+    "Pan shot", "Parabolic mic", "Pitch meeting", "Post-production", 
+    "Practical effects", "Pre-production", "Principal photography", "Production assistant", 
+    "Production designer", "Pyrotechnics", "Rough cut", "Rushes", "Screen test", 
+    "Shooting script", "Shot list", "Slapstick", "Slow burn", "Soundstage", 
+    "Steadicam operator", "Storyboard artist", "Supporting actor", "Sweeps", 
+    "Telephoto lens", "Third act twist", "Tracking shot", "Underexposed", 
+    "Vanity project", "Visual storytelling", "Widescreen format", "Wrap party", 
+    "Zoom-in", "Zoom-out", "Action film", "Actor's guild", "Backstory", 
+    "Bit part", "Blocking", "Camera rig", "Chiaroscuro", "Color grading", 
+    "Composite shot", "Continuity error", "Depth of field", "Docudrama", 
+    "Dolly shot", "Double exposure", "Drive-in theater", "Dubbed film", 
+    "Dystopian", "Eyeline match", "Extreme wide shot", "Film noir", 
+    "Foley stage", "Frame rate", "Freeze frame", "Handheld camera", "High-angle shot", 
+    "Interior shot", "Keyframe", "Match cut", "MOS (Mit Out Sound)", "Nonlinear editing", 
+    "Over-the-shoulder shot", "Post-sync", "Previs", "Production budget", 
+    "Punchline", "Rack focus", "Reverse angle", "Slate marker", "Soft focus", 
+    "Split screen", "Steadicam", "Title sequence", "Tracking shot", "Underexposed shot"
+]
+
+impossible = [
+    "Aerial shot", "Antihero", "Back projection", "Boom operator", "Bit part", 
+    "Blocking", "Cameo appearance", "Chiaroscuro", "Color correction", 
+    "Composite shot", "Continuity error", "Crossfade", "Depth of field", 
+    "Diegetic sound", "Distant shot", "Docudrama", "Double exposure", 
+    "Dutch angle", "Dynamic range", "Eyeline match", "Extreme close-up", 
+    "Fast motion", "Film analysis", "Film buff", "Film rating", "Flash forward", 
+    "Foley stage", "Freeze frame", "Golden age", "Grips", "Handheld camera", 
+    "High frame rate", "High-key lighting", "Jump cut", "Jump scare", "Lavalier mic", 
+    "Low-angle shot", "Low light", "Macro shot", "Method acting", 
+    "Non-diegetic sound", "Non-linear narrative", "Overdub", "Overexposure", 
+    "Parabolic mic", "Post-production", "Rack focus", "Red carpet event", 
+    "Rushes", "Screen actor", "Screen test", "Second unit", "Steadicam", 
+    "Storyboard artist", "Tropes", "Underexposure", "Vanity project", 
+    "Visual storytelling", "Walk-on part", "White balance", "Zoom dolly", 
+    "Wide-angle shot", "Widescreen format", "Workprint", "Wrap shot", 
+    "Zoom lens", "Underexposed shot", "Undercranking", "Vignette", 
+    "Vertical integration", "Multi-camera", "Mise en scène", "Oscillating fan", 
+    "Over-the-shoulder shot", "Pre-production", "Keyframe", "Rotoscope", 
+    "SAG (Screen Actors Guild)", "Slugline", "Soft focus", "Static shot", 
+    "Split diopter", "Spotting session", "Telephoto lens", "Vanity project", 
+    "Walk-on role", "Wardrobe stylist", "Zoom-in", "Zoom-out", "Undercranking", 
+    "Wide-angle shot", "Three-act structure", "Zoom dolly"
+]
+
+
+
+movies = [easy, medium, difficult, professional, impossible]
